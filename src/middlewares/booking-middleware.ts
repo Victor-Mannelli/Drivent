913a3b0be39756ce86db0de561f96599744c3f4c
Victor-Mannelli <imp.mannelli@gmail.com>
2023-02-11 17:35:23 -0300

    feat: add validation for changing booked room route

--- src/middlewares/booking-middleware.ts
+++ src/middlewares/booking-middleware.ts
@@ -5,14 +5,27 @@ import httpStatus from "http-status";
 
 export async function validateGetBooking(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const userId: number = req.userId;
-  const bookingData = await bookingService.getBookingByUserId(userId);
 
-  if (bookingData === null) return res.sendStatus(httpStatus.NOT_FOUND);
+  const userBookings = await bookingService.getBookingByUserId(userId);
+  if (userBookings === null) return res.sendStatus(httpStatus.NOT_FOUND);
 
   next();
 }
 export async function validatePostBooking(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const roomId: number = req.body.roomId;
+
+  const Room = await bookingService.getRoomById(roomId);
+  if (Room === null) return res.sendStatus(httpStatus.NOT_FOUND);
+  if (Room.capacity !== 0) return res.sendStatus(httpStatus.FORBIDDEN);
+
+  next();
+}
+export async function validateRoomChanges(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const userId: number = req.userId;
+  const roomId: number = req.body.roomId;
+
+  const userBookings = await bookingService.getBookingByUserId(userId);
+  if (userBookings === null) return res.sendStatus(httpStatus.NOT_FOUND);
 
   const Room = await bookingService.getRoomById(roomId);
   if (Room === null) return res.sendStatus(httpStatus.NOT_FOUND);
--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { authenticateToken, validateBody, validateGetBooking, validatePostBooking } from "@/middlewares";
+import { authenticateToken, validateBody, validateGetBooking, validatePostBooking, validateRoomChanges } from "@/middlewares";
 import { getBookingByUserId, postBooking } from "@/controllers";
 import { roomIdSchema } from "@/schemas/booking-schemas";
 
@@ -9,5 +9,6 @@ bookingRouter
   .all("/*", authenticateToken)
   .get("/", validateGetBooking, getBookingByUserId)
   .post("/", validateBody(roomIdSchema), validatePostBooking, postBooking)
+  .put("/:bookingId", validateBody(roomIdSchema), validateRoomChanges);
 
 export { bookingRouter };
