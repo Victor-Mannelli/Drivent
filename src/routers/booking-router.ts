37782a36137c71af52a9e4c000c581268f2137b9
Victor-Mannelli <imp.mannelli@gmail.com>
2023-02-11 17:24:27 -0300

    fix: controller wasn't added in the post route for booking

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticateToken, validateBody, validateGetBooking, validatePostBooking } from "@/middlewares";
-import { getBookingByUserId } from "@/controllers";
+import { getBookingByUserId, postBooking } from "@/controllers";
 import { roomIdSchema } from "@/schemas/booking-schemas";
 
 const bookingRouter = Router();
@@ -8,6 +8,6 @@ const bookingRouter = Router();
 bookingRouter
   .all("/*", authenticateToken)
   .get("/", validateGetBooking, getBookingByUserId)
-  .post("/", validateBody(roomIdSchema), validatePostBooking);
+  .post("/", validateBody(roomIdSchema), validatePostBooking, postBooking)
 
 export { bookingRouter };
